1b0fe1f7f260ccc99844c8abf327df135b3d5b08
omrobbie <putu.roby@yahoo.com>
2017-09-28 11:07:56 +0800

    modify detail layout

--- app/src/main/java/com/omrobbie/cataloguemovie/DetailActivity.java
+++ app/src/main/java/com/omrobbie/cataloguemovie/DetailActivity.java
@@ -2,12 +2,23 @@ package com.omrobbie.cataloguemovie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class DetailActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
